cb7b1ef7579cad8cdbe3904881c96ddba5b290a2
JohnSnoow <jean-bezet@orange.fr>
2016-03-12 16:52:07 +0100

    Remplissage de Lieu

--- persistent_src/objective/Lieu.java
+++ persistent_src/objective/Lieu.java
@@ -1,5 +1,21 @@
 package objective;
 
 public class Lieu {
+	String titre;
+	float lattitude;
+	float longitude;
+	
+	// GETTERS :
+	
+	public String getTitre () {
+		return this.titre;
+	};
+	
+	public float getLattitude () {
+		return this.lattitude;
+	};
 
+	public float getLongitude () {
+		return this.longitude;
+	};
 }
